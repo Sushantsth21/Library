320fa13e016523efd6a16c31a01c05a2099d179a
Sushant Shrestha <sushant.shrestha003@gmail.com>
2024-02-23 16:26:19 -0500

    created an index for the objects in the array

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-function Book(author, title, pages_num, read) {
+function Book(author, title, pages_num, read,index) {
     this.author = author;
     this.title = title;
     this.pages_num = pages_num;
     this.read = read;
+    this.index = index;
 }
 
 const myLibrary = [];
@@ -17,14 +18,31 @@ function addBookToLibrary(){
         let title = document.getElementById("title").value;
         let numPages = document.getElementById("numPages").value;
         let read = document.querySelector('input[name="read"]:checked').value;
+        let index = myLibrary.length+1;
         
-        const newBook = new Book(author, title, numPages, read);
+        const newBook = new Book(author, title, numPages, read,index);
         myLibrary.push(newBook);
+        console.log(newBook.index);
     
         document.getElementById("myForm").reset();
+        displayBooks(newBook);
     });
     
 }
+
+function displayBooks(book){
+    document.getElementById("books").innerHTML += `
+    <div class="book">
+        <ul>
+            <li>Author: ${book.author}<li>
+            <li>Title: ${book.title}<li>
+            <li>Number of pages: ${book.pages_num}<li>
+            <li>Read: ${book.read}<li>
+        <ul>
+    <div>`
+    
+}
+
 
 addBookToLibrary();
 
